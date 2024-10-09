ea095d7bb8cc44deddd92396834af04b029be440
Torwent <Torwent@users.noreply.github.com>
2024-10-09 20:51:33 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { $ } from "bun"
 import { Hono } from "hono"
 import { serveStatic } from "hono/bun"
 
-const output = await $`cd static/layers-osrs && bun install && bun start`.text()
-console.log(output)
+//const output = await $`cd static/layers-osrs && bun install && bun start`.text()
+//console.log(output)
 
 const app = new Hono()
 
